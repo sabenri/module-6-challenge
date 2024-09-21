c95e8e3a430c82f9c789d7b07d005b54830614b4
Sabely Sabely <enriquezsabely@gmail.com>
2024-09-21 18:17:36 -0400

    added 2nd function

--- assets/script.js
+++ assets/script.js
@@ -9,3 +9,17 @@ function removewithfilter(arr) {
     let outputCities = arr.filler((v, i, self) => i === self.indexOf(v));
     return outputCities;
 }
+
+function displayPastCities () {
+    const uniqueCities = removewithfilter(cities);
+    $("#stored-cities").empty();
+    for (var i = 0; i < Math.min(5, uniqueCities.length); i++) {
+        let pastcity = $("<button>").text(uniqueCities[i]);
+        pastcity.on('click', function (event) {
+            event.preventDefault();
+            getCitiesLonlat(cityName);
+            $("#weather-today").empty();
+            $("#cards").empty();
+        });
+    }
+}
