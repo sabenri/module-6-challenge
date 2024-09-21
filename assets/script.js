5fd08bac7839ab15673079008ce56f205c1826f6
Sabely Sabely <enriquezsabely@gmail.com>
2024-09-21 18:05:10 -0400

    added the frist function

--- assets/script.js
+++ assets/script.js
@@ -4,3 +4,8 @@ let cities = JSON.parse(localStorage.getItem("city")) || [];
 let outputCities = [];
 const searchButton = $("#search-button");
 let cityInput =$("city-input");
+
+function removewithfilter(arr) {
+    let outputCities = arr.filler((v, i, self) => i === self.indexOf(v));
+    return outputCities;
+}
